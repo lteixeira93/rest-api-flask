196207d5edefe20c5bb58c793008b23c6e462fa0
Luiz <lteixeira93@outlook.com>
2024-04-07 13:11:11 +0100

    fix: Connection refused with mongo

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    "db": "users",
+    "port": 27017,
+    "host": "mongodb",
+    "username": "admin",
+    "password": "admin"
+}
 api = Api(app)
+db = MongoEngine(app)
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(unique=True, required=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
     """Get all user from DB"""
 
     def get(self):
-        return {"message": "user 1"}
+        return jsonify(UserModel.objects())  # {"message": "user 1"}
 
 
 class User(Resource):
